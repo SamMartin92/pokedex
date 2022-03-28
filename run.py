d0d183008817b02ab3dd62df0437df5d40cf044f
SamMartin92 <samm92@live.ie>
2022-03-28 16:02:47 +0000

    adds in landing page

--- run.py
+++ run.py
@@ -1,3 +1,66 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import requests
+import time
+import pokebase as pb
+""""
+value = 'bulbasaur'
+response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{value}")
+pokedex_data = response.json()
+print(pokedex_data)"""
+
+def run_landing_page():
+    print(" _____     ____    _  __  ______   _____    ______  __   __")
+    print("|  __ \   / __ \  | |/ / |  ____| |  __ \  |  ____| \ \ / /")
+    print("| |__) | | |  | | | ' /  | |__    | |  | | | |__     \ V / ")
+    print("|  ___/  | |  | | |  <   |  __|   | |  | | |  __|     > <  ")
+    print("| |      | |__| | | . \  | |____  | |__| | | |____   / . \ ")
+    print("|_|       \____/  |_|\_\ |______| |_____/  |______| /_/ \_\ ")                                                   
+    
+
+
+    print('''
+    ##################################################################
+    ##################################################################
+
+
+
+        
+    {{MMMMMMMMMMMMMMMMMMMMMWNOdc,.                       .,cd0NWMMMMMMMMMMMMMMMMMMMMM
+    MMMMMMMMMMMMMMMMMMWXkc'.           .........           .'cxXWMMMMMMMMMMMMMMMMMM
+    MMMMMMMMMMMMMMMMNOc.        ...'',,;;;;;;;;;,,''...        .cONMMMMMMMMMMMMMMMM
+    MMMMMMMMMMMMMMNx;.      ..',;;;;;;;;;;;;;;;;;;;;;;;,...      .;xNMMMMMMMMMMMMMM
+    MMMMMMMMMMMMNk;      ..,;;;;;;;;;;;;;;;;;;;;;;;;;;;;;,,'..      ;kNMMMMMMMMMMMM
+    MMMMMMMMMMW0c.     .';;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;,,,'.     .:0WMMMMMMMMMM
+    MMMMMMMMMWx'     .';;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;,,,,'.     .xWMMMMMMMMM
+    MMMMMMMMNo.     .;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;,,,,,'.    .oNMMMMMMMM
+    MMMMMMMNl.    .';;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;,,,,,'.    .oNMMMMMMM
+    MMMMMMNd.    .,;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;,,,,,,.    .dWMMMMMM
+    MMMMMMk.    .,;;;;;;;;;;;;;;;;;;;;,'.......',;;;;;;;;;;;;;;;,,,,,,.    .kMMMMMM
+    MMMMMX:     ';;;;;;;;;;;;;;;;;;'..           ..';;;;;;;;;;;;;,,,,,'.    ;XMMMMM
+    MMMMMx.    .;;;;;;;;;;;;;;;;;,.     .';;;'.     .,;;;;;;;;;;;,,,,,,.    .xMMMMM
+    MMMMNc    .';;;;;;;;;;;;;;;;'.   .:kKNWWWNKk:.   .,;;;;;;;;;;;,,,,,'.    cNMMMM
+    MMMMK;    ..''''''''''''''''.   .xNMMMMMMMMMNd.   .''''''''''''......    ;KMMMM
+    MMMM0'                          cNMMMMMMMMMMMNc                          '0MMMM
+    MMMM0'                          cNMMMMMMMMMMMNc                          '0MMMM
+    MMMMK;    :OOOOOOOOOOOOOOOOk,   .dNMMMMMMMMMNx.   ,kOOOOOOOOOkxxxxxo.    ;KMMMM
+    MMMMNc    :NMMMMMMMMMMMMMMMM0,   .:kKNWWWNKk:.   ,0MMMMMMMMMWNXXXXXx.    cNMMMM
+    MMMMMx.   .kMMMMMMMMMMMMMMMMWKc.    .',;;'.    .cKWMMMMMMMMMWNXXXXKc    .xMMMMM
+    MMMMMX;    :XMMMMMMMMMMMMMMMMMWOl'.         .'lOWMMMMMMMMMMMNXXXXXx.    ;XMMMMM
+    MMMMMMk.   .oNMMMMMMMMMMMMMMMMMMMN0xolccclox0NMMMMMMMMMMMMMWNXXXXO,    .kMMMMMM
+    MMMMMMWd.   .oNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWNXXXXO,    .dWMMMMMM
+    MMMMMMMNo.   .lXMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWNXXXXk'    .oNMMMMMMM
+    MMMMMMMMNo.    ;OWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWNXXX0o.    .oNMMMMMMMM
+    MMMMMMMMMWx'    .lKWMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWNXXKx,     'xWMMMMMMMMM
+    MMMMMMMMMMWKc.    .l0WMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMWNNX0d,     .cKWMMMMMMMMMM
+    MMMMMMMMMMMMNk;     .:xKWMMMMMMMMMMMMMMMMMMMMMMMMMMMWNKkl'      ;kNMMMMMMMMMMMM
+    MMMMMMMMMMMMMMNk;.     .:dOXWMMMMMMMMMMMMMMMMMMMMWX0dc'.     .;kNMMMMMMMMMMMMMM
+    MMMMMMMMMMMMMMMMNOl.       .;ldk0KNNWWWWWWWNXKOxo:'.       .cONMMMMMMMMMMMMMMMM
+    MMMMMMMMMMMMMMMMMMWXkc'.         ..',,,,,,,'..         .'ckXWMMMMMMMMMMMMMMMMMM
+    MMMMMMMMMMMMMMMMMMMMMWN0dc,.                       .,cd0NWMMMMMMMMMMMMMMMMMMMMM
+    
+    
+    ###############################################################################
+    ############################################################}}#################
+        ''')
+
+
+run_landing_page()
