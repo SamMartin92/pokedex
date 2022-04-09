8b74af6d5b66e13b6c9cd70d03c6a13a4e65dbb9
SamMartin92 <samm92@live.ie>
2022-04-09 19:15:11 +0000

    sets up APIS and grants access to pokedex_data google sheet

--- run.py
+++ run.py
@@ -3,9 +3,21 @@ import time
 import pokebase as pb
 import os
 import random
+import gspread
+from google.oauth2.service_account import Credentials
 
-
+#Constant variables
 GENERATION = 1
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('pokedex_sheet')
+
 gen_resource = None
 
 
